2e183cf9a25f7e5338cf3de3d26329973a2fbdf2
lepLinus <linus.lepschies@gmail.com>
2020-10-20 15:21:17 +0200

    Update

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayText : MonoBehaviour
+{
+    public TextMeshProUGUI Maintext;
+    public string[] Alldialogs;
+    int index;
+    bool isrunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        StartCoroutine(Displaytext(Alldialogs[index]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !isrunning)
+        {
+            isrunning = true;
+            Maintext.text = "";
+            index++;
+            StartCoroutine(Displaytext(Alldialogs[index]));
+        }
+    }
+
+    public IEnumerator Displaytext(string text)
+    {
+        char[] c = text.ToCharArray();
+        for (int i = 0; i< c.Length; i++)
+        {
+            Maintext.text += c[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(2);
+        Maintext.text = "";
+        isrunning = false;
+    }
+}
