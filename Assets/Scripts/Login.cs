5a3db93480a3e8111e5d07453bd3299ac0ac6522
lepLinus <49990732+lepLinus@users.noreply.github.com>
2020-11-03 18:12:17 +0100

    Update

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    Getrequest getrequest;
+    [Header("Input Field for Login")]
+    public TMP_InputField NameInput,PasswdInput;
+    [Header("Text for Error and Debug on User UI")]
+    public TextMeshProUGUI ErrorText;
+
+    void Start()
+    {
+        getrequest = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Getrequest>();
+    }
+    public void StartLogin()
+    {
+        StartCoroutine(LoginSend());
+    }
+    // Update is called once per frame
+    public IEnumerator LoginSend() {
+        string Name = NameInput.text;
+        string Passwort = PasswdInput.text;
+
+        if (Name == "" || Name == null)
+        {
+            ErrorText.text = "Name kann nicht leer sein";
+
+        }else if (Passwort == "" || Passwort == null)
+        {
+            ErrorText.text = "Passwort kann nicht leer sein";
+        }
+        getrequest.Get("https://linuslepschies.de/login.php?Name=" + Name + "&PassWD="+ Passwort.GetHashCode());
+        yield return new WaitForSeconds(2);
+        if (getrequest.Message.Length == 0)
+        {
+            ErrorText.text = "Fehler beim anmelden" + getrequest.Message;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManager>().Login(Name);
+        }
+    }
+}
