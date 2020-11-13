eed70d2529d49a3967dfea35abdc54319f46417f
lepLinus <49990732+lepLinus@users.noreply.github.com>
2020-11-13 23:14:13 +0100

    Update

---
+++ Assets/Scripts/RankSystem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class RankSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string Message;
+    public GameObject Menu;
+    public TextMeshProUGUI RankNames;
+    public TextMeshProUGUI Exp;
+
+    public void Start()
+    {
+        Message = null;
+    }
+
+    public void Open()
+    {
+        Menu.SetActive(true);
+        StartCoroutine(OpenInner());
+    }
+
+    public IEnumerator OpenInner()
+    {
+        Message = null;
+        StartCoroutine(GetRequest("https://www.linuslepschies.de/PhpGallerie/Rank.php?PassWD=1MRf!s13"));
+        yield return new WaitForSeconds(1);
+        if (Message.Length == 0 || Message == "[]")
+        {
+            Debug.Log("Error on Getting data");
+        }
+        else
+        {
+            string josn = "{\"Items\":" + Message + "}";
+            RankInfo[] userInfos = JsonHelper.FromJson<RankInfo>(josn);
+            RankNames.text = "";
+            Exp.text = "";
+            for (int i = 0; i< userInfos.Length; i++)
+            {
+                RankNames.text += userInfos[i].UserName + "\n" + "\n";
+                Exp.text += userInfos[i].Exp + " Inspiration" + "\n" + "\n";
+            }
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            webRequest.certificateHandler = new AcceptAllCertificatesSignedWithASpecificKeyPublicKey();
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                try
+                {
+                    //Message = System.Text.Encoding.UTF8.GetString(webRequest.downloadHandler.data, 3, webRequest.downloadHandler.data.Length - 3);
+                    Message = webRequest.downloadHandler.text;
+                }
+                catch (Exception e)
+                {
+                    Message = webRequest.downloadHandler.text;
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class RankInfo
+    {
+        public string UserName;
+        public string Exp;
+        public static RankInfo CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<RankInfo>(jsonString);
+        }
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject VolumGlob;
+    public Slider Slider;
+    public AudioSource Main;
+
+    public void SetRes(int index)
+    {
+        if (index == 1)
+        {
+            Screen.SetResolution(1920, 1080, true);
+        }
+        else if (index == 2)
+        {
+            Screen.SetResolution(3840, 2160, true);
+        }
+        else
+        {
+            Screen.SetResolution(1280, 720, true);
+        }
+    }
+    public void SetQul(int index)
+    {
+        if (index == 1)
+        {
+            VolumGlob.SetActive(true);
+        }
+        else
+        {
+            VolumGlob.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        Main.volume = Slider.value;
+    }
+}
