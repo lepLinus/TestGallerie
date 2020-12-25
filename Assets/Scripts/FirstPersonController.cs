9f993c0f1492423775cea96e388f6aa5de1797f4
lepLinus <49990732+lepLinus@users.noreply.github.com>
2020-12-25 23:42:40 +0100

    Update

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform playerCamera;
+
+    public float gravity = -13.0f;
+    public float walkSpeed = 6.0f;
+    public float mouseSensitivity = 3.5f;
+    [Range(0.0f, 0.5f)] public float moveSmoothTime = 0.2f;
+    [Range(0.0f, 0.5f)] public float mouseSmoothTime = 0.03f;
+    
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+
+    Vector2 currentDir, currentDirVelocity;
+    Vector2 currentMouseDelta, currentMouseDeltaVelocity;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+        UpdateMouseLook();
+    }
+
+    void UpdateMovement()
+    {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0.0f;
+        }
+
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90, 90);
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    public Animator animator;
+
+    [Header("Preferences")]
+    float moveSpeed;
+    public float walkSpeed;
+    public float backSpeed;
+    public float runSpeed;
+    public float transitionSpeed;
+    public float staminaIncrease;
+    public float staminaDecrease;
+
+    public bool controlled;
+
+    public Transform waypoints;
+
+    Vector3 direction;
+
+    void Start()
+    {
+        controlled = true;
+
+        transform.position = waypoints.GetChild(Random.Range(0, waypoints.childCount)).position;
+    }
+
+    void Update()
+    {
+        //If Player has no control quit
+        if (!controlled)
+        {
+            return;
+        }
+
+        //Input
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        //Set Direction
+        direction = Vector3.ClampMagnitude(transform.forward * yInput + transform.right * xInput, 1);
+
+        //Run and Animation
+        if (Input.GetKey(KeyCode.LeftShift) && yInput > 0 )
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, runSpeed, transitionSpeed * Time.deltaTime);
+        }
+        else if (yInput > 0)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, transitionSpeed * Time.deltaTime);
+        }
+        else if (yInput < 0)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, backSpeed, transitionSpeed * Time.deltaTime);
+        }
+
+
+        //Animation
+        animator.SetFloat("yDir", yInput > 0 ? yInput * moveSpeed : yInput * moveSpeed * 2);
+        animator.SetFloat("xDir", xInput * 5);
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        GetComponent<Rigidbody>().MovePosition(transform.position + direction * moveSpeed * Time.fixedDeltaTime);
+    }
+}
